43203060cb15d95f4113e7ee2b2d4711e8a41fa5
andreszapataq <andreszapataq@gmail.com>
2021-07-15 00:27:58 -0500

    add form (course)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
+import { useState } from "react"
 import Button from "./Button"
 
 const Header = ({ title }) => {
+    const [count, setCount] = useState(0)
+    
     const onClick = () => {
-        console.log('Click')
+        setCount(count + 1)
+        console.log(count)
     }
 
     return (
         <header className='header'>
             <h1>{title}</h1>
             <Button text='Add' onClick={onClick} />
+            <p>You clicked {count} times</p>
         </header>
     )
 }
