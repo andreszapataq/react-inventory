86f0ee266dcd4810bac9809fb2a7b7b5fe835fb4
andreszapataq <andreszapataq@gmail.com>
2021-10-23 11:35:48 -0500

    filter by asesor_id

--- server/controllers/inventarioController.js
+++ server/controllers/inventarioController.js
@@ -1,14 +1,16 @@
+const { ObjectId } = require('bson');
 const Inventario = require('../models/inventarioModel');
 
 exports.getTest = async (req, res, next) => {
     const inventario = await Inventario.aggregate([
-        {"$match": {"asesor_id": 1}},
+        {"$match": {"asesor_id": ObjectId("612489d0d8c6c1e3d2f6302d")}},
         {"$unwind": "$stock"},
         {"$match": {"stock.cantidad": {"$gt": 0}}},
         {"$group": {
             "_id": "$_id",
             "nombre": {"$first": "$nombre"},
-            "stock": {"$push": "$stock"}}}
+            "stock": {"$push": "$stock"},
+            "asesor_id": {"$first": "$asesor_id"}}}
     ]).sort({"_id": 1});
 
     /* {"asesor_id": 1}, {"stock": {"$elemMatch": {"cantidad": {"$ne": 0}}}} */
