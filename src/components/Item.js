57fe058a3f6f3108e08a2303afbb9b3bfef6d48f
andreszapataq <andreszapataq@gmail.com>
2021-10-24 20:36:09 -0500

    item styling

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,13 @@
 const Item = ({ item }) => {
     return (
         <div className='item'>
-            <h3>{item.item}</h3>
-            <p>{item.codigo}</p>
-            <p>{item.cantidad}</p>
+            <div>
+                <h3>{item.item}</h3>
+                <p>{item.codigo}</p>
+            </div>
+            <div className='item-cantidad'>
+                <p>{item.cantidad}</p>
+            </div>
         </div>
     )
 }
