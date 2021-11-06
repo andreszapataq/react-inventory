931cf4223cdf9d626a2e950cf1d9688d58a6989e
andreszapataq <andreszapataq@gmail.com>
2021-11-05 22:04:34 -0500

    useLocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import { useLocation } from 'react-router-dom'
+
 const Header = ({ title }) => {
+    let location = useLocation()
+    
     return (
         <header className='header'>
-            <p>Hola</p>
+            {location.pathname === "/" && <p>Hola</p>}
             <h1>{title}</h1>
         </header>
     )
