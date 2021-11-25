cc0dfeca09c6f37ada0ba3fa758e869efe1ed799
andreszapataq <andreszapataq@gmail.com>
2021-11-24 21:46:14 -0500

    continue Routes component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,10 +1,49 @@
-import { Switch } from "react-router-dom" // REACT ROUTER OR DOM?
+import { Redirect, Switch } from "react-router-dom" // REACT ROUTER OR DOM?
+import Layout from "../layout/Layout"
 
 const Routes = () => {
+    const PrivateRoute = ({ component: Component, ...rest }) => {
+        if(sessionExpired) {
+          setSessionExpired(true)
+          return <Login />
+        }
+        return (
+          <Route
+            {...rest}
+            render={(props) =>
+              isLogged ? (
+                <div className="App">
+                  <Layout>
+                    <Component {...props} />
+                  </Layout>
+                </div>
+              ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
+        )
+    }
+
+    const PublicRoute = ({ component: Component, restricted, ...rest }) => {
+        return (
+          <Route
+            {...rest}
+            render={(props) =>
+              isLogged && restricted ? (
+                <Redirect to={props.path} />
+              ) : (
+                <Component {...props} />
+              )
+            }
+          />
+        )
+    }
+    
     return (
         <div>
             <Switch>
-                
+
             </Switch>
         </div>
     )
