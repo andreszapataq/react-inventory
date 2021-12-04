60bdedb0dc2211c3ef5141b1772b16de7cebde72
andreszapataq <andreszapataq@gmail.com>
2021-12-04 01:14:33 -0500

    App logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Routes from "./components/Routes"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Layout from "./layout/Layout"
+import Login from "./components/Login"
+import Bodegas from "./components/Bodegas"
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes />
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<Layout />}>
+              <Route path="/" element={<Bodegas />} />
+            </Route>
+        </Routes>
       </div>
     </Router>
   )
