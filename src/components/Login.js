356e2f55c5985d48a8e88d6cda7d4c012e4683eb
andreszapataq <andreszapataq@gmail.com>
2021-12-07 23:49:49 -0500

    begin wiht Login component styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
             <input type="text" />
             <input type="password" />
             <Link to="/">
-                <button type="button">Entrar</button>
+                <button type="button" className="btn">Entrar</button>
             </Link>
         </div>
     )
