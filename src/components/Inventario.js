75b16db66ddbac4ab7efe9729f8c0ad0def9d8fd
andreszapataq <andreszapataq@gmail.com>
2021-12-26 23:41:47 -0500

    clear code and merge

--- src/components/Inventario.js
+++ src/components/Inventario.js
@@ -1,40 +1,41 @@
-import { Link, useLocation } from "react-router-dom";
-import Item from "./Item";
+import { Link, useLocation } from "react-router-dom"
+import Item from "./Item"
 
 const Inventario = () => {
-  let location = useLocation();
+  let location = useLocation()
   const bodega = location.state.bodega;
 
-  let stock = bodega.stock;
-  console.log("gz stock:", stock);
+  let stock = bodega.stock
+  console.log(stock)
 
-  const newStock = [];
-  let uniqueCodigos = [];
+  const newStock = []
+  const uniqueCodigos = []
 
   stock.forEach((item) => {
-    // si el codigo existe en el array, +1 la cantidad
-    if (uniqueCodigos.includes(item.codigo)) {
+    // Si el codigo existe en el array, +1 la cantidad
+    if(uniqueCodigos.includes(item.codigo)) {
       newStock.forEach((newItem) => {
-        if (newItem.codigo === item.codigo) {
-          newItem.cantidad = newItem.cantidad + 1;
+        if(newItem.codigo === item.codigo) {
+          newItem.cantidad = newItem.cantidad + 1
         }
-      });
+      })
     } else {
-      // si no existe en el array, agragarlo con cantidad 1
-      newStock.push({
-        nombre: item.nombre,
-        codigo: item.codigo,
-        cantidad: 1,
-      });
-      uniqueCodigos.push(item.codigo);
+        // Si no existe en el array, agregarlo con cantidad 1
+        newStock.push({
+            codigo: item.codigo,
+            nombre: item.nombre,
+            cantidad: 1
+        })
+        uniqueCodigos.push(item.codigo)
     }
-  });
+  })
+
+  newStock.sort((a, b) => (a.codigo > b.codigo ? 1 : -1))
+  console.log(newStock)
 
   return (
     <div>
-      {newStock
-        .sort((a, b) => (a.codigo > b.codigo ? 1 : -1))
-        .map((item, index) => (
+      {newStock.map((item, index) => (
           <div key={index}>
             <Link to={`/lotes/${index}`} state={{ item }}>
               <Item key={index} item={item} />
