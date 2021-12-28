317b7c4d670f6f9342b9996254bb23f85808e437
andreszapataq <andreszapataq@gmail.com>
2021-12-28 16:43:30 -0500

    useLocation in Lotes component

--- src/components/Lotes.js
+++ src/components/Lotes.js
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom"
+
 const Lotes = () => {
+    let location = useLocation()
+    const item = location.state.item
+
+    console.log(item)
+    
     return (
-        <div>
-            <p>Lote</p>
+        <div className='item'>
+            <h3>Lote</h3>
+            <p>Fecha Vencimiento</p>
         </div>
     )
 }
