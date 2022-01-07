ede821f3a9293fbba5d7c188d39af918779c17b9
andreszapataq <andreszapataq@gmail.com>
2022-01-06 22:46:50 -0500

    header done!

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -9,22 +9,26 @@ const Header = ({ asesor }) => {
     return (
         <header className='header'>
             {location.pathname === "/" && (
-                <div>
+                <div className="header-row-1">
                     <p>Hola</p>
                     <h1 className="text-asesor">{asesor}</h1>
                 </div>
             )}
-            {bodega && location.pathname.includes('/inventario/') && <h1 className="text-bodega">{bodega}</h1>}
+            {bodega && location.pathname.includes('/inventario/') &&
+                <div className="header-row-1">
+                    <h1 className="text-bodega">{bodega}</h1>
+                </div>   
+            }
             {item &&
-                <div className="header-lotes">
-                    <div>
+                <>
+                    <div className="header-row-1">
                         <h1 className="text-producto">{item}</h1>
                         <p>{codigo}</p>
                     </div>
-                    <div className="header-lotes-bodega">
+                    <div className="header-row-2">
                         <p className="text-bodega-lotes">Ahora en... {bodega}</p>
                     </div>
-                </div>
+                </>
             }
         </header>
     )
