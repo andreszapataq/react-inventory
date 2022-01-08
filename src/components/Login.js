0ce1508eb9319c712156251d762a3d6ef9495990
andreszapataq <andreszapataq@gmail.com>
2022-01-07 23:04:13 -0500

    add logo img

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom"
 const Login = ({ login }) => {
     return (
         <div className='login'>
+            <div className="login-brand">
+                <a href="/">
+                    <img src="" alt="Logo" />
+                </a>
+            </div>
             <input type="text" />
             <input type="password" />
             <Link to="/" onClick={login}>
