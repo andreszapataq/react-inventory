830ee73eba8a0478a904145b764c09cab424dfe1
andreszapataq <andreszapataq@gmail.com>
2022-01-16 18:50:07 -0500

    first commit

--- src/components/Inventario.js
+++ src/components/Inventario.js
@@ -1,17 +1,39 @@
+import { useState, useEffect } from "react"
 import { Link, useLocation } from "react-router-dom"
 import Item from "./Item"
 
 const Inventario = () => {
   let location = useLocation()
-  const bodega = location.state?.bodega;
+  // let bodega = location.state.bodega;
+  const [bodega, setBodega] = useState([])
+  const [newStock, setNewStock] = useState([])
 
-  let stock = bodega?.stock
-  console.log(stock)
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://localhost:5004/api/v1/inventario')
+      let datax = await res.json()
+      console.log(datax.data[location.pathname.split('/').pop()])
+      setBodega(datax.data[location.pathname.split('/').pop()])
+    }
+    
+    if(!location.state) {
+      fetchData()
+    }
+    else {
+      setBodega(location.state.bodega)
+    }
+  }, [location.state])
 
-  const newStock = []
-  const uniqueCodigos = []
+  useEffect(() => {
+    if(bodega.stock) {
+      funcShit()
+    }
+  }, [bodega])
 
-  stock.forEach((item) => {
+  const funcShit = () => {
+    const uniqueCodigos = []
+
+  bodega.stock.forEach((item) => {
     // Si el codigo existe en el array, +1 la cantidad
     if(uniqueCodigos.includes(item.codigo)) {
       newStock.forEach((newItem) => {
@@ -34,6 +56,7 @@ const Inventario = () => {
 
   newStock.sort((a, b) => (a.codigo > b.codigo ? 1 : -1))
   console.log(newStock)
+  }
 
   return (
     <div>
