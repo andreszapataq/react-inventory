b6bd637308f96e943ada3fb34137eea3b4f90191
andreszapataq <andreszapataq@gmail.com>
2022-01-17 23:16:47 -0500

    fix lotes path

--- src/components/Lotes.js
+++ src/components/Lotes.js
@@ -2,11 +2,11 @@ import { useLocation } from "react-router-dom"
 
 const Lotes = () => {
     let location = useLocation()
-    const item = location.state.item
+    const item = location.state?.item
     
     return (
         <div>
-            {item.lotes.map((lotes, index) => (
+            {item?.lotes.map((lotes, index) => (
                 <div className='item' key={index}>
                     <h3>{lotes.lote}</h3>
                     <p>{lotes.fecha_vencimiento}</p>
