5f491025641e5a09e312efd705118e182df93516
andreszapataq <andreszapataq@gmail.com>
2022-01-24 23:16:54 -0500

    jwt install and implement

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const jwt = require('jsonwebtoken')
 const inventarioRouter = require('./routes/inventarioRoutes');
 const usuarioRouter = require('./routes/usuarioRoutes')
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,15 +1,21 @@
+const jwt = require('jsonwebtoken')
 const Usuario = require('../models/usuarioModel')
 
 exports.getLogged = async (req, res, next) => {
     const user = await Usuario.findOne({email: req.body.email })
     if(!user) return res.status(400).send('Email no encontrado')
 
+    console.log(user)
+
     const validPass = await (req.body.password === user.password)
     if(!validPass) return res.status(400).send('Password incorrecto')
 
-    res.status(200).json({
+    const token = jwt.sign({_id: user._id}, process.env.SECRET_TOKEN)
+    res.header('auth-token', token).send(token)
+
+    /* res.status(200).json({
         status: "Epa!",
         message: "Ok McKain",
         data: "logged"
-    })
+    }) */
 }
