0893a0ee042881c602c0a784fbda9ff4362b27a3
andreszapataq <andreszapataq@gmail.com>
2022-02-10 23:41:19 -0500

    add data-result div

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,8 @@
-const SearchBar = ({ placeholder }) => {
+const SearchBar = ({ placeholder, data }) => {
   return (
     <div className="search-wrapper">
         <input type="text" className="input-search" placeholder={placeholder} />
+        <div className="data-result"></div>
     </div>
   )
 }
