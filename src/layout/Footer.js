d951f7a5c48d62ee3a187ca632838634bd343471
andreszapataq <andreszapataq@gmail.com>
2022-02-14 23:09:21 -0500

    add crear bodega and home button

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons"
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
+
 const Footer = ({ logout }) => {
     return (
         <footer>
-            <p onClick={logout} className="footer-logout">Cerrar sesión</p>
-            <p>Powered by Ness Digital</p>
+            <div className="plus-icon">
+                <FontAwesomeIcon icon={faCirclePlus} className="fa-2x" />
+            </div>
+            <div className="footer-copy">
+                <p onClick={logout} className="footer-logout-text">Cerrar sesión</p>
+                <p className="footer-creator-text">Powered by Ness Digital</p>
+            </div>
+            <div className="home-icon">
+                <Link to="/">
+                    <FontAwesomeIcon icon={faHouse} className="fa-2x" />
+                </Link>
+            </div>
         </footer>
     )
 }
