1112214a229e9fb3e6beec6105bcf6442e178164
andreszapataq <andreszapataq@gmail.com>
2022-02-18 21:53:54 -0500

    filtering bodegas working

--- src/components/Bodegas.js
+++ src/components/Bodegas.js
@@ -1,10 +1,31 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 import Bodega from "./Bodega"
 
-const Bodegas = ({ bodegas }) => {
+const Bodegas = ({ bodegas, placeholder }) => {
+    const [searchTerm, setSearchTerm] = useState('')
+    
     return (
         <div>
-            {bodegas.map((bodega, index) => (
+            <div className="search-inputs">
+                <div className="search-icon">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </div>
+                <input type="text" className="input-search" placeholder={placeholder} onChange={(e) => {
+                setSearchTerm(e.target.value)
+                }} />
+            </div>
+            {bodegas.filter((val) => {
+                if(searchTerm === '') {
+                    return val
+                }
+                else if(val.nombre.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    return val
+                }
+            }).map((bodega, index) => (
                 <div key={bodega._id}>
                     <Link to={`/inventario/${index}`} state={{bodega}}>
                         <Bodega bodega={bodega} />
--- src/layout/PrivateLayout.js
+++ src/layout/PrivateLayout.js
@@ -1,14 +1,14 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import Header from './Header'
-import SearchBar from '../components/SearchBar'
+// import SearchBar from '../components/SearchBar'
 import Footer from './Footer'
 
 const PrivateLayout = ({ asesor, data, isLogged, logout }) => {
-    console.log(data)
+    console.log(data) // ELIMINAR
     return isLogged ? (
         <div className='private-layout'>
             <Header asesor={asesor} />
-            <SearchBar placeholder="Buscar..." data={data} />
+            {/* <SearchBar placeholder="Buscar..." data={data} /> */}
             <Outlet />
             <Footer logout={logout} />
         </div>
