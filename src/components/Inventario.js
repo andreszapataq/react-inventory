d9411ecee69ead58ad78e174795dbc09369ad6eb
andreszapataq <andreszapataq@gmail.com>
2022-02-24 17:58:03 -0500

    use SearchBar with Inventario

--- src/components/Inventario.js
+++ src/components/Inventario.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom"
 import Item from "./Item"
+
+import SearchBar from "./SearchBar";
 
 const Inventario = () => {
   let location = useLocation()
@@ -7,36 +10,51 @@ const Inventario = () => {
 
   let stock = bodega?.stock
 
-  const newStock = []
-  const uniqueCodigos = []
+  const [stox, setStox] = useState(stock)
+  const [filteredBodegas, setFilteredBodegas] = useState(stock)
 
-  stock?.forEach((item) => {
-    // Si el codigo existe en el array, +1 la cantidad
-    if(uniqueCodigos.includes(item.codigo)) {
-      newStock.forEach((newItem) => {
-        if(newItem.codigo === item.codigo) {
-          newItem.cantidad = newItem.cantidad + 1
-          newItem.lotes.push({lote: item.lote, fecha_vencimiento: item.fecha_vencimiento})
-        }
-      })
-    } else {
-        // Si no existe en el array, agregarlo con cantidad 1
-        newStock.push({
-            codigo: item.codigo,
-            nombre: item.nombre,
-            lotes: [{lote: item.lote, fecha_vencimiento: item.fecha_vencimiento}],
-            cantidad: 1
+  const handleChange = (filteredData) => {
+      setFilteredBodegas(filteredData)
+  }
+
+  useEffect(() => {
+      setFilteredBodegas(stock)
+  }, [stock])
+
+  useEffect(() => {
+    const newStock = []
+    const uniqueCodigos = []
+        
+    stock?.forEach((item) => {
+      // Si el codigo existe en el array, +1 la cantidad
+      if(uniqueCodigos.includes(item.codigo)) {
+        newStock.forEach((newItem) => {
+          if(newItem.codigo === item.codigo) {
+            newItem.cantidad = newItem.cantidad + 1
+            newItem.lotes.push({lote: item.lote, fecha_vencimiento: item.fecha_vencimiento})
+          }
         })
-        uniqueCodigos.push(item.codigo)
-    }
-  })
-
-  newStock.sort((a, b) => (a.codigo > b.codigo ? 1 : -1))
+      } else {
+          // Si no existe en el array, agregarlo con cantidad 1
+          newStock.push({
+              codigo: item.codigo,
+              nombre: item.nombre,
+              lotes: [{lote: item.lote, fecha_vencimiento: item.fecha_vencimiento}],
+              cantidad: 1
+          })
+          uniqueCodigos.push(item.codigo)
+      }
+    })
+  
+    newStock.sort((a, b) => (a.codigo > b.codigo ? 1 : -1))
+    
+    setStox(newStock)
+  }, [])
 
   return (
     <div className="inventario-section">
-      {/* <SearchBar data={bodegas} handleChange={handleChange} /> */}
-      {newStock.map((item, index) => (
+      <SearchBar data={stock} handleChange={handleChange} />
+      {filteredBodegas.map((item, index) => (
           <div key={index}>
             <Link to={`/lotes/${index}`} state={{ item, bodega }}>
               <Item key={index} item={item} />
