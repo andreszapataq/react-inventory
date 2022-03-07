0395ab589e4b14ff262a045f9b5e993393783b9c
andreszapataq <andreszapataq@gmail.com>
2022-03-07 14:03:50 -0500

    SearchBar in Lotes

--- src/components/Lotes.js
+++ src/components/Lotes.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
+
+import SearchBar from "./SearchBar"
 
 const Lotes = () => {
     let location = useLocation()
     const item = location.state?.item
+
+    console.log(item)
+
+    const [filteredBodegas, setFilteredBodegas] = useState(item)
+
+    const handleChange = (filteredData) => {
+        setFilteredBodegas(filteredData)
+    }
+
+    useEffect(() => {
+        setFilteredBodegas(item)
+    }, [item])
     
     return (
         <div className="list-section">
-            {item?.lotes.map((lotes, index) => (
+            <SearchBar data={item.lotes} handleChange={handleChange} filterBy='lote' />
+            {filteredBodegas.lotes.map((lotes, index) => (
                 <div className='item' key={index}>
                     <h3 className="text-lote-xl">{lotes.lote}</h3>
                     <p>{lotes.fecha_vencimiento}</p>
