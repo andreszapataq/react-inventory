f9cc16e4c4038e2821defdc5f4788ff97e67bbc7
Gustavo Zapata <tavordie@hotmail.com>
2022-03-07 21:15:20 +0000

    fix searchbar for lotes and inventario

--- src/components/Inventario.js
+++ src/components/Inventario.js
@@ -1,66 +1,68 @@
 import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom"
-import Item from "./Item"
+import { Link, useLocation } from "react-router-dom";
+import Item from "./Item";
 
 import SearchBar from "./SearchBar";
 
 const Inventario = () => {
-  let location = useLocation()
+  let location = useLocation();
   const bodega = location.state?.bodega;
+  let stock = bodega?.stock;
 
-  let stock = bodega?.stock
-
-  const [stox, setStox] = useState(stock)
-  const [filteredBodegas, setFilteredBodegas] = useState(stock)
+  const [filteredBodegas, setFilteredBodegas] = useState([]);
 
   const handleChange = (filteredData) => {
-      setFilteredBodegas(filteredData)
-  }
+    parseStock(filteredData); // TODO: El resultado habia que parsearlo otra vez
+  };
 
   useEffect(() => {
-      setFilteredBodegas(stox)
-  }, [stox])
+    parseStock(stock);
+  }, [stock]);
 
-  useEffect(() => {
-    const newStock = []
-    const uniqueCodigos = []
-        
-    stock?.forEach((item) => {
+  const parseStock = (stockArg) => {
+    const newStock = [];
+    const uniqueCodigos = [];
+
+    stockArg?.forEach((item) => {
       // Si el codigo existe en el array, +1 la cantidad
-      if(uniqueCodigos.includes(item.codigo)) {
+      if (uniqueCodigos.includes(item.codigo)) {
         newStock.forEach((newItem) => {
-          if(newItem.codigo === item.codigo) {
-            newItem.cantidad = newItem.cantidad + 1
-            newItem.lotes.push({lote: item.lote, fecha_vencimiento: item.fecha_vencimiento})
+          if (newItem.codigo === item.codigo) {
+            newItem.cantidad = newItem.cantidad + 1;
+            newItem.lotes.push({
+              lote: item.lote,
+              fecha_vencimiento: item.fecha_vencimiento,
+            });
           }
-        })
+        });
       } else {
-          // Si no existe en el array, agregarlo con cantidad 1
-          newStock.push({
-              codigo: item.codigo,
-              nombre: item.nombre,
-              lotes: [{lote: item.lote, fecha_vencimiento: item.fecha_vencimiento}],
-              cantidad: 1
-          })
-          uniqueCodigos.push(item.codigo)
+        // Si no existe en el array, agregarlo con cantidad 1
+        newStock.push({
+          codigo: item.codigo,
+          nombre: item.nombre,
+          lotes: [
+            { lote: item.lote, fecha_vencimiento: item.fecha_vencimiento },
+          ],
+          cantidad: 1,
+        });
+        uniqueCodigos.push(item.codigo);
       }
-    })
-  
-    newStock.sort((a, b) => (a.codigo > b.codigo ? 1 : -1))
-    
-    setStox(newStock)
-  }, [])
+    });
+
+    newStock.sort((a, b) => (a.codigo > b.codigo ? 1 : -1));
+    setFilteredBodegas(newStock);
+  };
 
   return (
     <div className="list-section">
-      <SearchBar data={stock} handleChange={handleChange} filterBy='codigo' />
+      <SearchBar data={stock} handleChange={handleChange} filterBy="nombre" />
       {filteredBodegas.map((item, index) => (
-          <div key={index}>
-            <Link to={`/lotes/${index}`} state={{ item, bodega }}>
-              <Item key={index} item={item} />
-            </Link>
-          </div>
-        ))}
+        <div key={index}>
+          <Link to={`/lotes/${index}`} state={{ item, bodega }}>
+            <Item key={index} item={item} />
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Lotes.js
+++ src/components/Lotes.js
@@ -1,35 +1,36 @@
-import { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
-import SearchBar from "./SearchBar"
+import SearchBar from "./SearchBar";
 
 const Lotes = () => {
-    let location = useLocation()
-    const item = location.state?.item
+  let location = useLocation();
+  const item = location.state?.item;
+  const [filteredBodegas, setFilteredBodegas] = useState([]);
 
-    console.log(item)
+  useEffect(() => {
+    setFilteredBodegas(item.lotes); // TODO: Esta es la solucion
+  }, [item]);
 
-    const [filteredBodegas, setFilteredBodegas] = useState(item)
+  const handleChange = (filteredData) => {
+    setFilteredBodegas(filteredData);
+  };
 
-    const handleChange = (filteredData) => {
-        setFilteredBodegas(filteredData)
-    }
-
-    useEffect(() => {
-        setFilteredBodegas(item)
-    }, [item])
-    
-    return (
-        <div className="list-section">
-            <SearchBar data={item.lotes} handleChange={handleChange} filterBy='lote' />
-            {filteredBodegas.lotes.map((lotes, index) => (
-                <div className='item' key={index}>
-                    <h3 className="text-lote-xl">{lotes.lote}</h3>
-                    <p>{lotes.fecha_vencimiento}</p>
-                </div>
-            ))}
+  return (
+    <div className="list-section">
+      <SearchBar
+        data={item.lotes}
+        handleChange={handleChange}
+        filterBy="lote"
+      />
+      {filteredBodegas?.map((el, index) => (
+        <div className="item" key={index}>
+          <h3 className="text-lote-xl">{el.lote}</h3>
+          <p>{el.fecha_vencimiento}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Lotes
+export default Lotes;
