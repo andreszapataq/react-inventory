040cdc5a211a4f72d3faf939e38a26432e5b12b9
andreszapataq <andreszapataq@gmail.com>
2022-04-03 23:59:24 -0500

    lp class

--- src/components/LotePage.js
+++ src/components/LotePage.js
@@ -6,16 +6,22 @@ const LotePage = () => {
 
   return (
     <div className="LotePage">
-      <h3>Bodega actual</h3>
-      <p>{bodega}</p>
-      <h3>Trasladar a...</h3>
-      <select name="bodega" id="bodega">
-        <option value="bodega-1">Bodega 1</option>
-        <option value="bodega-2">Bodega 2</option>
-        <option value="bodega-3">Bodega 3</option>
-      </select>
-      <h3>Notas</h3>
-      <textarea name="notas" id="notas" cols="50" rows="4"></textarea>
+      <div className="lp-title">
+        <h3>Bodega actual</h3>
+        <p>{bodega}</p>
+      </div>
+      <div className="lp-selection">
+        <h3>Trasladar a...</h3>
+        <select name="bodega" id="bodega">
+          <option value="bodega-1">Bodega 1</option>
+          <option value="bodega-2">Bodega 2</option>
+          <option value="bodega-3">Bodega 3</option>
+        </select>
+      </div>
+      <div className="lp-notes">
+        <h3>Notas</h3>
+        <textarea name="notas" id="notas" cols="50" rows="4"></textarea>
+      </div>
     </div>
   )
 }
