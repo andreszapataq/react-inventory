2e443aed847a0d291d008fcc7f64266e6bd06ced
andreszapataq <andreszapataq@gmail.com>
2022-04-08 23:59:27 -0500

    CrearBodega asesor

--- src/components/CrearBodega.js
+++ src/components/CrearBodega.js
@@ -3,6 +3,12 @@ const CrearBodega = () => {
     <div>
       <h3>Nombre de bodega</h3>
       <input type="text" />
+      <h3>Asesor</h3>
+      <select>
+        <option></option>
+        <option></option>
+        <option></option>
+      </select>
     </div>
   )
 }
