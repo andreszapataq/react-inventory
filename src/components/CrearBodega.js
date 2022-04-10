99a76e64d863060319d7c22b6097dc9446d87ca1
andreszapataq <andreszapataq@gmail.com>
2022-04-09 23:39:13 -0500

    CrearBodega ready all content

--- src/components/CrearBodega.js
+++ src/components/CrearBodega.js
@@ -3,6 +3,13 @@ const CrearBodega = () => {
     <div>
       <h3>Nombre de bodega</h3>
       <input type="text" />
+      <h3>Asesor</h3>
+      <select name="asesor" id="asesor">
+        <option value="daniel-oliveros">Daniel Oliveros</option>
+        <option value="vive-solutions">Vive Solutions</option>
+      </select>
+      <button className="btn btn-grey">Cancelar</button>
+      <button className="btn btn-blue">Guardar</button>
     </div>
   )
 }
