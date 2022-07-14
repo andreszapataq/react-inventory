61f6213dd550ce47cfb039e38634af9cd135d994
andreszapataq <andreszapataq@gmail.com>
2022-07-13 22:18:09 -0500

    generate object from Inventario

--- src/components/Inventario.js
+++ src/components/Inventario.js
@@ -30,7 +30,11 @@ const Inventario = () => {
 
     console.log(codigos)
 
-    codigos.forEach((x) => { counts[x] = (counts[x] || 0) + 1 })
+    codigos.forEach((x) => { 
+      counts.push({
+        codigo: counts[x] = (counts[x] || 0) + 1 
+      }) 
+    })
     
     console.log(counts)
 
