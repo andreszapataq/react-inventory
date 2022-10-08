7b4c84b355453c406ddd1f6d4ea4d5b6932ae434
andreszapataq <andreszapataq@gmail.com>
2022-10-07 22:19:08 -0500

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif']
+    },
+    colors: {
+      blue: 'steelblue'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
